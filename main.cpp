a2620421faff668ac8dc969e59a5bf85543fd880
hufeiyu <hufeiyu@DESKTOP-S8JBIQ7.localdomain>
2022-01-27 12:17:11 +0800

    add daemon

--- main.cpp
+++ main.cpp
@@ -2,10 +2,55 @@
 #include "MySocket.h"
 #include "Desk.h"
 #include "Mahjong.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 using namespace std;
 
 int main() {
+
+    //开启守护进程
+    printf("开启守护进程执行，请到./logs/文件夹下查看日志\n");
+
+    // 创建子进程
+    pid_t pid = fork();
+    if(pid > 0) {
+        exit(0);//父进程退出
+    }
+    
+    // 子进程重新创建会话，脱离当前控制终端
+    setsid();
+
+    //设置掩码
+    umask(022);
+
+    //更改工作目录
+    chdir("./log/");
+
+    //关闭或者重定向文件描述符
+    // //关闭
+    // int fd = open("/dev/null", O_RDWR);
+    // dup2(fd, STDIN_FILENO);
+    // dup2(fd, STDOUT_FILENO);
+    // dup2(fd, STDERR_FILENO);
+
+    //重定向,打印输出到日志文件中
+    int fd = open("./logs/log.txt", O_RDWR | O_APPEND | O_CREAT);
+    dup2(fd, STDIN_FILENO);
+    dup2(fd, STDOUT_FILENO);
+    dup2(fd, STDERR_FILENO);
+
+    //启动服务器逻辑
+
     Controller *controller = new Controller();
     controller->start();
     int num = 0;
@@ -16,5 +61,6 @@ int main() {
 
 
     getchar();
+    close(fd);
     return 0;
 }
